21c67ecce6f9e6c7b624b3a577c7898322e40a1a
62656E <bet.travis36@live.com>
2024-12-04 17:45:17 +0000

    Refactor Streamlit app for improved readability and Gantt chart display logic

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 # Import necessary libraries
-import streamlit as st 
+import streamlit as st
 from io import BytesIO
 from gantt import createGantt
 import plotly as py
@@ -8,12 +8,16 @@ import plotly as py
 st.title("Gantt Chart Maker")
 
 # Prompt the user with the file format and purpose of the app
-st.markdown("""
+st.markdown(
+    """
 This app allows you to create a Gantt chart using the `plotly` library. Please upload an xlsx file with the following columns; Task, Stage, StartDate, EndDate, CompletionFrac and Title. To customise stage colours, add a column named `StageColor` with the colour of the stage in hex format. To customise legend order, add a column named `LegendOrder` with the order of the legend.
-""")
+"""
+)
 
 # Upload the xlsx file
-uploaded_file = st.file_uploader("Choose xlsx file", type="xlsx", accept_multiple_files=False)
+uploaded_file = st.file_uploader(
+    "Choose xlsx file", type="xlsx", accept_multiple_files=False
+)
 
 # Check if the file has been uploaded
 if uploaded_file is not None:
@@ -22,16 +26,18 @@ if uploaded_file is not None:
 else:
     st.write("Please upload an xlsx file")
 
-# Display the Gantt chart
-st.plotly_chart(GanttChart)
-    
+# Display the Gantt chart if it has been created
+if GanttChart is type(py.graph_objs._figure.Figure):
+    st.plotly_chart(GanttChart)
+elif img_bytes is type(BytesIO):
+    st.image(img_bytes)
+else:
+    st.write("Please upload an xlsx file")
+
 # Display download button
 st.download_button(
     label="Download Gantt Chart as PNG",
     data=img_bytes,
     file_name="gantt_chart.png",
-    mime="image/png"
+    mime="image/png",
 )
-
-            
-            
